6f378727aed4e053e0adecbb327a6cae076f355c
pavanreddy1895-dotco <pavanreddy1895@gmail.com>
2026-03-30 22:32:47 +0530

    Create process.py

---
+++ process.py
@@ -0,0 +1,7 @@
+class Process:
+    def __init__(self, pid, cpu_burst, memory, priority):
+        self.pid = pid
+        self.cpu_burst = cpu_burst
+        self.memory = memory
+        self.priority = priority
+        self.waiting_time = 0
